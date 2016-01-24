3b38e155f08b77e813989d08654bc58db0a57323
Kevin Lanzon <kevinlanzon@gmail.com>
2016-01-24 17:00:59 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+CloudMargin JavaScript Test
+========
+
---
+++ spec/roundTo2Spec.js
@@ -0,0 +1,10 @@
+describe("#roundTo2Places", function () {
+
+  it("should round 100.056 to 100.06", function () {
+    expect(roundTo2Places(100.056)).toEqual(100.06);
+  });
+
+  it("should round 100.054 to 100.05", function () {
+    expect(roundTo2Places(100.054)).toEqual(100.05);
+  });
+});
