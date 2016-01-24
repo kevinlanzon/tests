c29d256f3c3c215537e9a1e818abaf741c7d4c01
Kevin Lanzon <kevinlanzon@gmail.com>
2016-01-24 17:04:54 +0000

    adds failing unit test to roundTo2Places spec

--- spec/roundTo2Spec.js
+++ spec/roundTo2Spec.js
@@ -7,4 +7,8 @@ describe("#roundTo2Places", function () {
   it("should round 100.054 to 100.05", function () {
     expect(roundTo2Places(100.054)).toEqual(100.05);
   });
+
+  it("should round 100.06555 to 100.07", function () {
+    expect(roundTo2Places(100.06555)).toEqual(100.07);
+  });
 });
